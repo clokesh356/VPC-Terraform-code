1d8b901322f22a2f027566d914ba398beef629ee
unknown <clokeshreddy356@gmail.com>
2022-09-15 11:35:53 +0530

    updated grafana in apache

--- apache.tf
+++ apache.tf
@@ -52,3 +52,16 @@ resource "aws_instance" "apache" {
     Name = "stage-apache"
   }
 }
+
+#extra for practice
+resource "aws_instance" "grafana" {
+  ami           = "ami-0b89f7b3f054b957e"
+  instance_type = "t2.micro"
+  #   vpc_id =aws_vpc.vpc.id
+  subnet_id              = aws_subnet.private[0].id
+  vpc_security_group_ids = [aws_security_group.apache.id]
+
+  tags = {
+    Name = "stage-grafana"
+  }
+}
